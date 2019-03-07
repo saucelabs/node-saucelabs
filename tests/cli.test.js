0b34c982c174bb1ea189dcc0a457cebe5770e716
christian-bromann <mail@christian-bromann.com>
2019-03-07 14:51:01 +0100

    finish unit tests

---
+++ tests/cli.test.js
@@ -0,0 +1,65 @@
+import yargs from 'yargs'
+
+import { run } from '../src/cli'
+
+jest.mock('../src/index.js', () => {
+    class SauceLabsMock {
+        constructor (options) {
+            this.options = options
+            this.listBuilds = jest.fn().mockReturnValue(Promise.resolve(options.returnValue))
+        }
+    }
+
+    return SauceLabsMock
+})
+
+test('should be able to execute a command', () => {
+    run()
+    expect(yargs.usage).toBeCalledTimes(1)
+    expect(yargs.epilog).toBeCalledTimes(1)
+    expect(yargs.demandCommand).toBeCalledTimes(1)
+    expect(yargs.help).toBeCalledTimes(1)
+    expect(yargs.option).toBeCalledWith('user', {
+        alias: 'u',
+        name: 'user',
+        description: 'your Sauce Labs username'
+    })
+
+    const [command, description, handler, cb] = yargs.command.mock.calls[4]
+    expect(command).toBe('listBuilds <username> [limit] [subaccounts]')
+    expect(description).toBe('Get all of a users builds')
+
+    handler(yargs)
+    expect(yargs.positional)
+        .toBeCalledWith('username', { describe: 'username', type: 'string' })
+    expect(yargs.positional).toBeCalledWith('limit', {
+        describe: 'Number of results to return',
+        type: 'number',
+        default: 50
+    })
+    expect(yargs.positional).toBeCalledWith('subaccounts', {
+        describe: 'Include subaccounts in list of jobs',
+        type: 'boolean',
+        default: false
+    })
+
+    const params = {
+        user: 'foobaruser',
+        key: 'barfookey',
+        headless: true,
+        region: 'anywhere',
+        username: 'username-param',
+        limit: 42,
+        subaccounts: true,
+        returnValue: 'some result'
+    }
+    const api = cb(params)
+
+    expect(api.options).toEqual({
+        user: 'foobaruser',
+        key: 'barfookey',
+        headless: true,
+        region: 'anywhere',
+    })
+    expect(api.listBuilds).toBeCalledWith('username-param', params)
+})
