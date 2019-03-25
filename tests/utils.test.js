2baa704dca3b9bc85660d1401fae05513d414a9a
François Martin <fracoisma@hotmail.com>
2019-03-25 17:01:51 +0100

    Add strictSSL option to requests (#54)
    See https://github.com/webdriverio/webdriverio/pull/3758

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,4 @@
-import { createHMAC, getSauceEndpoint, toString, isValidType, getErrorReason } from '../src/utils'
+import { createHMAC, getSauceEndpoint, toString, isValidType, getErrorReason, getStrictSsl } from '../src/utils'
 
 test('createHMAC', async () => {
     expect(await createHMAC('foo', 'bar', 'loo123'))
@@ -50,4 +50,45 @@ test('getErrorReason', () => {
     expect(getErrorReason({ message: 'foobar', detail: 'barfoo' })).toBe('foobar')
     expect(getErrorReason('foobarloo')).toBe('foobarloo')
     expect(getErrorReason(['some reason', 'some other reason'])).toBe('some reason')
+})
+
+describe('getStrictSsl', () => {
+    beforeEach(function() {
+        delete process.env.STRICT_SSL
+        delete process.env.strict_ssl
+    })
+
+    it('should return "false" when environment variable "STRICT_SSL" is defined with value "false"', () => {
+        process.env['STRICT_SSL'] = 'false'
+        expect(getStrictSsl()).toEqual(false)
+    })
+
+    it('should return "false" when environment variable "strict_ssl" is defined with value "false"', () => {
+        process.env['strict_ssl'] = 'false'
+        expect(getStrictSsl()).toEqual(false)
+    })
+
+    it('should return "true" when environment variable "STRICT_SSL" is defined with value "true"', () => {
+        process.env['STRICT_SSL'] = 'true'
+        expect(getStrictSsl()).toEqual(true)
+    })
+
+    it('should return "true" when environment variable "strict_ssl" is defined with value "true"', () => {
+        process.env['strict_ssl'] = 'true'
+        expect(getStrictSsl()).toEqual(true)
+    })
+
+    it('should return "true" when environment variable "STRICT_SSL" / "strict_ssl" is not defined', () => {
+        expect(getStrictSsl()).toEqual(true)
+    })
+
+    it('should return "true" when environment variable "STRICT_SSL" is defined with any other value than "false"', () => {
+        process.env['STRICT_SSL'] = 'foo'
+        expect(getStrictSsl()).toEqual(true)
+    })
+
+    it('should return "true" when environment variable "strict_ssl" is defined with any other value than "false"', () => {
+        process.env['strict_ssl'] = 'foo'
+        expect(getStrictSsl()).toEqual(true)
+    })
 })
