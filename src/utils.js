5c58e9e37ed4aeb8e36ecbecc8a6fb59ab036050
martinfrancois <fracoisma@hotmail.com>
2019-03-25 15:44:30 +0100

    add util method `getStrictSsl`

--- src/utils.js
+++ src/utils.js
@@ -122,3 +122,11 @@ export function getErrorReason (body = {}) {
 
     return body.message || body.detail || 'unknown'
 }
+
+/**
+ * If the environment variable "STRICT_SSL" is defined as "false", it doesn't require SSL certificates to be valid.
+ * This is used in requests to define the value of the "strictSSL" option.
+ */
+export function getStrictSsl() {
+    return !(process.env.STRICT_SSL === 'false' || process.env.strict_ssl === 'false')
+}
