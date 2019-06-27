a590bf6c396d4bf0fac20a9e3e43d2f8013bef84
Vildan Softic <vsoftic@ranorex.com>
2019-06-27 08:51:14 +0200

    chore(linter): fix linter issues

--- scripts/generate-typings.js
+++ scripts/generate-typings.js
@@ -1,19 +1,19 @@
-const fs = require("fs");
-const changeCase = require("change-case");
-const CodeGen = require("swagger-typescript-codegen").CodeGen;
+const fs = require('fs')
+const changeCase = require('change-case')
+const CodeGen = require('swagger-typescript-codegen').CodeGen
 
-const file = "./apis/sauce.json";
-const swagger = JSON.parse(fs.readFileSync(file, "UTF-8"));
+const file = './apis/sauce.json'
+const swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'))
 const tsSourceCode = CodeGen.getTypescriptCode({
-    className: "SauceLabs",
+    className: 'SauceLabs',
     swagger,
     template: {
-        class: fs.readFileSync("./scripts/class.mustache", "utf-8"),
-        method: fs.readFileSync("./scripts/method.mustache", "utf-8"),
+        class: fs.readFileSync('./scripts/class.mustache', 'utf-8'),
+        method: fs.readFileSync('./scripts/method.mustache', 'utf-8'),
     }
-});
+})
 
 const caseChangedSource = tsSourceCode
-    .replace(/~~~(.*)~~~/g, (_, group) => changeCase.camel(group));
+    .replace(/~~~(.*)~~~/g, (_, group) => changeCase.camel(group))
 
-fs.writeFileSync("build/index.d.ts", caseChangedSource, { encoding: "utf-8" })
+fs.writeFileSync('build/index.d.ts', caseChangedSource, { encoding: 'utf-8' })
