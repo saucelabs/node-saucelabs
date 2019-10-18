d2acfbc95c11df31e9e13b61e46755189e9ba64f
christian-bromann <mail@christian-bromann.com>
2019-10-18 10:18:37 +0300

    better handle unknown descriptions

--- src/cli.js
+++ src/cli.js
@@ -16,7 +16,12 @@ export const run = () => {
             p.required ? `<${p.name}>` : `[${p.name}]`
         )).join(' ')}`
 
-        yargs.command(command.trim(), options.description.summary, (yargs) => {
+        const description = (
+            options.description.summary ||
+            options.description.description ||
+            'Unknown description'
+        )
+        yargs.command(command.trim(), description, (yargs) => {
             for (const param of params) {
                 const paramDescription = {
                     describe: param.description,
