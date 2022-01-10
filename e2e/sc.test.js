46bf8a234f1b9cd2bc70a7e74e0a45c1d09da166
Christopher Hiller <boneskull@boneskull.com>
2022-01-10 14:47:37 -0800

    test: try to simplify some things

--- e2e/sc.test.js
+++ e2e/sc.test.js
@@ -7,10 +7,11 @@ jest.setTimeout(60 * 1000) // 60s should be sufficient to boot SC
 /**
  * unmock
  */
-jest.unmock('form-data')
-jest.unmock('got')
-jest.unmock('yargs')
-jest.unmock('zlib')
+jest.unmock('download')
+    .unmock('form-data')
+    .unmock('got')
+    .unmock('yargs')
+    .unmock('zlib')
 
 test('should not be able to run Sauce Connect due to invalid credentials', async () => {
     const api = new SauceLabs({ key: 'foobar' })
@@ -33,6 +34,6 @@ test('should be able to run Sauce Connect', async () => {
 
 afterAll(async () => {
     // this is here because for whatever reason, we're logging output from 
-    // sc _after_ sc.close() has fulfilled.
+    // sc _after_ sc.close() has fulfilled. this is disallowed by jest, for whatever reason
     await new Promise((resolve) => setTimeout(resolve, 5000))
 })
--- tests/sauceConnectLoader.test.js
+++ tests/sauceConnectLoader.test.js
@@ -2,98 +2,97 @@ import * as utils from '../src/utils'
 import SauceConnectLoader from '../src/sauceConnectLoader'
 import { promises } from 'fs'
 import download from 'download'
-describe('constructor', () => {
-    test('should throw if platform is unsupported', () => {
-        jest.spyOn(utils, 'getPlatform').mockImplementation(() => 'whatever')
-        expect(
-            () => new SauceConnectLoader({ sauceConnectVersion: '1.2.3' })
-        ).toThrow(ReferenceError, 'Unsupported platform whatever')
-    })
 
-    afterEach(() => {
-        utils.getPlatform.mockRestore()
-    })
-})
-
-describe('verifyAlreadyDownloaded()', () => {
-    beforeEach(() => {
-        jest.spyOn(
-            SauceConnectLoader.prototype,
-            '_download'
-        ).mockImplementation(() => Promise.resolve())
-    })
-
-    afterEach(() => {
-        SauceConnectLoader.prototype._download.mockRestore()
-    })
-
-    describe('when executable exists', () => {
-        beforeEach(() => {
-            jest.spyOn(promises, 'stat').mockImplementation(() =>
-                Promise.resolve()
+describe('SauceConnectLoader', () => {
+    describe('constructor', () => {
+        test('should throw if platform is unsupported', () => {
+            jest.spyOn(utils, 'getPlatform').mockImplementation(
+                () => 'whatever'
             )
-        })
-
-        afterEach(() => {
-            promises.stat.mockRestore()
-        })
-        test('should not attempt to download anything', async () => {
-            const scl = new SauceConnectLoader({ sauceConnectVersion: '1.2.3' })
-            await scl.verifyAlreadyDownloaded()
-            expect(scl._download).not.toHaveBeenCalled()
+            expect(
+                () => new SauceConnectLoader({ sauceConnectVersion: '1.2.3' })
+            ).toThrow(ReferenceError, 'Unsupported platform whatever')
         })
     })
 
-    describe('when executable does not exist', () => {
-        beforeEach(() => {
-            jest.spyOn(promises, 'stat').mockImplementation(() =>
-                Promise.reject({ code: 'ENOENT' })
-            )
+    describe('method', () => {
+        describe('verifyAlreadyDownloaded()', () => {
+            let scl
+
+            beforeEach(() => {
+                jest.spyOn(
+                    SauceConnectLoader.prototype,
+                    '_download'
+                ).mockImplementation(() => Promise.resolve())
+            })
+
+            describe('when executable exists', () => {
+                beforeEach(() => {
+                    jest.spyOn(promises, 'stat').mockImplementation(() =>
+                        Promise.resolve()
+                    )
+                    scl = new SauceConnectLoader({
+                        sauceConnectVersion: '1.2.3',
+                    })
+                })
+
+                test('should not attempt to download anything', async () => {
+                    await scl.verifyAlreadyDownloaded()
+                    expect(scl._download).not.toHaveBeenCalled()
+                })
+            })
+
+            describe('when executable does not exist', () => {
+                beforeEach(() => {
+                    jest.spyOn(promises, 'stat').mockImplementation(() =>
+                        Promise.reject({ code: 'ENOENT' })
+                    )
+                    scl = new SauceConnectLoader({
+                        sauceConnectVersion: '1.2.3',
+                    })
+                })
+
+                test('should attempt to download', async () => {
+                    await scl.verifyAlreadyDownloaded()
+                    expect(scl._download).toHaveBeenCalled()
+                })
+            })
+
+            describe('when something else is wrong', () => {
+                beforeEach(() => {
+                    jest.spyOn(promises, 'stat').mockImplementation(() =>
+                        Promise.reject(new Error())
+                    )
+                    scl = new SauceConnectLoader({
+                        sauceConnectVersion: '1.2.3',
+                    })
+                })
+
+                test('should reject', () => {
+                    return expect(
+                        scl.verifyAlreadyDownloaded()
+                    ).rejects.toThrow()
+                })
+            })
         })
 
-        afterEach(() => {
-            promises.stat.mockRestore()
-        })
-        
-        test('should attempt to download', async () => {
-            const scl = new SauceConnectLoader({ sauceConnectVersion: '1.2.3' })
-            await scl.verifyAlreadyDownloaded()
-            expect(scl._download).toHaveBeenCalled()
-        })
-    })
+        describe('_download()', () => {
+            let scl
 
-    describe('when something else is wrong', () => {
-        beforeEach(() => {
-            jest.spyOn(promises, 'stat').mockImplementation(() =>
-                Promise.reject(new Error())
-            )
-        })
-        
-        test('should reject', () => {
-            const scl = new SauceConnectLoader({ sauceConnectVersion: '1.2.3' })
-            return expect(scl.verifyAlreadyDownloaded()).rejects.toThrow()
-        })
-        
-    })
-})
+            beforeEach(() => {
+                jest.spyOn(promises, 'chmod').mockImplementation(() =>
+                    Promise.resolve()
+                )
+                scl = new SauceConnectLoader({ sauceConnectVersion: '1.2.3' })
+            })
 
-describe('_download()', () => {
-    beforeEach(() => {
-        jest.spyOn(promises, 'chmod').mockImplementation(() =>
-            Promise.resolve()
-        )
-    })
-
-    afterEach(() => {
-        promises.chmod.mockRestore()
-    })
-
-    test('should delegate to `download` pkg', async () => {
-        const scl = new SauceConnectLoader({ sauceConnectVersion: '1.2.3' })
-        await scl._download()
-        expect(download).toHaveBeenCalledWith(scl.url, scl.dest, {
-            extract: true,
-            strip: 1,
+            test('should delegate to `download` pkg', async () => {
+                await scl._download()
+                expect(download).toHaveBeenCalledWith(scl.url, scl.dest, {
+                    extract: true,
+                    strip: 1,
+                })
+            })
         })
     })
 })
