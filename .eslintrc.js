b83f734d25571888190fc08d83aedb3610f454a9
Christopher Hiller <boneskull@boneskull.com>
2022-05-06 11:59:22 -0700

    chore(): add automatic formatting
    This change:
    
    1. Adds `lint-staged` and `husky` to automatically format files as a pre-commit hook
    2. Upgrades `prettier`
    3. Moves `prettier` config into `package.json`
    4. Adds/updates `.prettierignore` & `.eslintignore`
    5. Adds a `.gitattributes` to ensure LF line endings across project
    6. Changes indenting rules from 4 spaces to 2
    7. Use semicolons and let prettier handle quotes
    8. Applies ESLint rules against `.eslintrc.js` itself
    9. Removes `tests/.eslintrc.js` and puts the config into the root

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,32 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings"
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
   ],
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2016,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
-    semi: ["error", "never"],
-    quotes: ["error", "single"],
-    indent: [2, 4],
-
-    "import/no-unresolved": [2, {commonjs: true, amd: true}],
-    "import/named": 2,
-    "import/namespace": 2,
-    "import/default": 2,
-    "import/export": 2
-  }
-}
+    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+  },
+  overrides: [
+    {
+      files: 'tests/**/*.js',
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
