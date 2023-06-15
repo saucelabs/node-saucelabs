c42b81083729cb07149b0e538d69ed05dbca8510
Diego Molina <diemol@users.noreply.github.com>
2023-06-15 11:25:07 +0200

    Avoid running SC test when the env var is not present (#160)
    * Update sc.test.js
    
    * Update sc.test.js

--- e2e/sc.test.js
+++ e2e/sc.test.js
@@ -1,6 +1,10 @@
 import SauceLabs from '../build';
 
 const ID = process.env.GITHUB_RUN_ID ?? '(local)';
+// Only run the test when the env var is present
+// in GitHub Actions, otherwise it fails for untrusted PRs/
+const SKIP_TEST = process.env.GITHUB_RUN_ID && !process.env.SAUCE_USERNAME;
+
 
 jest.setTimeout(60 * 1000); // 60s should be sufficient to boot SC
 
@@ -15,6 +19,9 @@ jest
   .unmock('zlib');
 
 test('should not be able to run Sauce Connect due to invalid credentials', async () => {
+  if (SKIP_TEST) {
+    return;
+  }
   const api = new SauceLabs({key: 'foobar'});
   const err = await api
     .startSauceConnect({
@@ -26,6 +33,9 @@ test('should not be able to run Sauce Connect due to invalid credentials', async
 });
 
 test('should be able to run Sauce Connect', async () => {
+  if (SKIP_TEST) {
+    return;
+  }
   const api = new SauceLabs();
   const sc = await api.startSauceConnect({
     logger: console.log.bind(console),
